cc77000f8273eb9c3ebd126ba6b825a1e34948d3
JaroStancik <stancikj@gmail.com>
2022-10-16 21:22:59 +0200

    Revert "First version."
    This reverts commit ce89c970eeff58d7d48076eb240983122fe4326c.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-
-import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +6,5 @@ import { AuthService } from './auth/auth.service';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private authService: AuthService, private router: Router) {}
-
-  onLogout() {
-    this.authService.logout();
-    this.router.navigateByUrl('/auth');
-  }
+  constructor() {}
 }
